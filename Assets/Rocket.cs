34ec53e17f9cb35c215cff26caee10d7af8862eb
Liquidlisa <lconners90@yahoo.com.au>
2018-06-18 12:13:47 +1000

    going shopping, lol

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,43 +1,74 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
     [SerializeField] float turnrate = 250f;
     [SerializeField] float thrustrate = 1000f;
     Rigidbody rigidBody;
     AudioSource audioSource;
+
+    enum State {Alive, Death, Transcending };
+    State state = State.Alive;
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == State.Alive)
+        {
         Thrust();
         Rotate();
+        }
+        else
+        {
+            audioSource.Stop();
+        }
 	}
 
     private void OnCollisionEnter(Collision collision)
     {
+        if(state != State.Alive)
+        {
+            return;
+        }
         switch (collision.gameObject.tag)
         {
             case "Friendly":
                 break;
+            case "Finish":
+                state = State.Transcending;
+                Invoke("LoadNextScene", 1f);
+                break;
             default:
-                print("not friendly");
+                state = State.Death;
+                Invoke("LoadStartScene", 1f);
                 break;
         }
 
     }
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+        state = State.Alive;
+    }
+
+    void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+        state = State.Alive;
+    }
+
     void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             float thrustnow = thrustrate * Time.deltaTime;
-                rigidBody.AddRelativeForce(Vector3.up * thrustnow);
+            rigidBody.AddRelativeForce(Vector3.up * thrustnow);
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
@@ -49,9 +80,9 @@ public class Rocket : MonoBehaviour {
             audioSource.Stop();
         }
     }
+
     void Rotate()
     {
-
         rigidBody.freezeRotation = true; // only turns with key press, not from collision.
         if (Input.GetKey(KeyCode.A))
         {
